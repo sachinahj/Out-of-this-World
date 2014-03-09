e61ab18aa4570ec4ebbf5a2677c9e05cb2f49a90
Sachin Ahuja <Sachin@sachin-ahujas-macbook-pro.local>
2014-03-09 15:36:41 -0500

    Initial Commit

---
+++ Out of this World/SAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SAAppDelegate.h
+//  Out of this World
+//
+//  Created by Sachin Ahuja on 3/9/14.
+//  Copyright (c) 2014 Sachin Ahuja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Out of this World/SAViewController.h
@@ -0,0 +1,13 @@
+//
+//  SAViewController.h
+//  Out of this World
+//
+//  Created by Sachin Ahuja on 3/9/14.
+//  Copyright (c) 2014 Sachin Ahuja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAViewController : UIViewController
+
+@end
